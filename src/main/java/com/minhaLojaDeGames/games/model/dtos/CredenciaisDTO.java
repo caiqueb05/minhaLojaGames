ab0f780fdf92a3656904128fbb9c5fec385b8abb
ronanbedia <ronan.bedia@gmail.com>
2021-10-07 15:05:32 -0300

    @ronanbedia[Added DTOs Credenciais and UsuarioLogin- 07.10.2021]

---
+++ src/main/java/com/minhaLojaDeGames/games/model/dtos/CredenciaisDTO.java
@@ -0,0 +1,45 @@
+package com.minhaLojaDeGames.games.model.dtos;
+
+public class CredenciaisDTO {
+
+	
+	private Long idUsuario;
+	private String nomeUsuario;
+	private String emailUsuario;
+	private String senhaUsuario;
+	private String tokenUsuario;
+	
+	
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+	public String getEmailUsuario() {
+		return emailUsuario;
+	}
+	public void setEmailUsuario(String emailUsuario) {
+		this.emailUsuario = emailUsuario;
+	}
+	public String getSenhaUsuario() {
+		return senhaUsuario;
+	}
+	public void setSenhaUsuario(String senhaUsuario) {
+		this.senhaUsuario = senhaUsuario;
+	}
+	public String getTokenUsuario() {
+		return tokenUsuario;
+	}
+	public void setTokenUsuario(String tokenUsuario) {
+		this.tokenUsuario = tokenUsuario;
+	}
+
+
+}
